58ffa4c479bfeafbf810a6613dfb72c3d7f2d15c
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-03-05 14:43:36 -0300

    Complete image shows with fixed size

--- app/src/main/java/com/eati/pexels/presentation/PhotosScreen.kt
+++ app/src/main/java/com/eati/pexels/presentation/PhotosScreen.kt
@@ -1,13 +1,11 @@
 package com.eati.pexels.presentation
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -24,10 +22,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun PhotosScreen(viewModel: PhotosViewModel) {
@@ -45,16 +45,14 @@ fun PhotosScreen(viewModel: PhotosViewModel) {
 
 @Composable
 fun SearchBar(buttonActivation: () -> Unit, searchText: String, resetText: () -> Unit, onValueChange: (String) -> Unit) {
-
     var buttonText by remember { mutableStateOf("Search") }
 
-   Text(text = "Android App",
-        textAlign = TextAlign.Center,
+    Text(text = "EATI Pexels",
+        textAlign = TextAlign.Start,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold,
         color = Color.White,
-        modifier = Modifier.padding(start = 80.dp, bottom = 20.dp, top = 20.dp))
-
+        modifier = Modifier.padding(start = 10.dp, bottom = 10.dp, top = 10.dp))
 
     Column(
         modifier = Modifier
@@ -64,17 +62,22 @@ fun SearchBar(buttonActivation: () -> Unit, searchText: String, resetText: () ->
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.6f)
+                .fillMaxHeight(0.7f)
         ) {
             TextField(
                 value = searchText,
                 placeholder = {
-                    Text(text = "Search")
+                    Text(text = "What do you want to see?")
                 },
                 onValueChange = onValueChange,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = MaterialTheme.colors.surface
+                ),
                 modifier = Modifier
-                    .size(260.dp, 120.dp)
-                    .padding(start = 8.dp)
+                    .heightIn(min = 56.dp)
+                    .padding(start = 10.dp)
+                    .padding(end = 10.dp)
+                    .clip(shape = RoundedCornerShape(8.dp))
             )
 
             Button(
@@ -90,7 +93,8 @@ fun SearchBar(buttonActivation: () -> Unit, searchText: String, resetText: () ->
                 modifier = Modifier
                     .fillMaxHeight()
                     .fillMaxWidth()
-                    .padding(end = 8.dp)
+                    .padding(end = 10.dp)
+                    .clip(shape = RoundedCornerShape(8.dp))
             ) {
                 Icon(
                     Icons.Filled.Search,
@@ -106,8 +110,17 @@ fun SearchBar(buttonActivation: () -> Unit, searchText: String, resetText: () ->
     }
 }
 
+fun Modifier.customAlertDialog(
+    photo: Photo
+): Modifier = this.then(
+    Modifier
+        .heightIn(max = photo.height.dp)
+        .widthIn(max = photo.width.dp)
+)
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun PhotoCard(photo: Photo, modifier: Modifier = Modifier) {
+fun PhotoCard(photo: Photo, photoHeight: Int, photoWidth: Int, modifier: Modifier = Modifier) {
     var showDialog by remember { mutableStateOf(false) }
 
     AsyncImage(
@@ -121,30 +134,66 @@ fun PhotoCard(photo: Photo, modifier: Modifier = Modifier) {
     )
 
     if (showDialog) {
-
         AlertDialog(
-            modifier = Modifier
-                .size(600.dp),
+            properties = DialogProperties(usePlatformDefaultWidth = false),
+            modifier = Modifier.customAlertDialog(photo),
             onDismissRequest = { showDialog = false },
-            title = { Text(text = "Photographer: "+photo.photographer, Modifier.padding(8.dp)) },
+            title = { Text(
+                text = ("Photographer: "+photo.photographer),
+                fontSize = 25.sp,
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier
+                    .padding(8.dp)) },
             text = {
                 Column (
                     Modifier
-                        .verticalScroll(rememberScrollState())
-                        .fillMaxWidth(),
+                        .verticalScroll(rememberScrollState()),
                     horizontalAlignment = Alignment.CenterHorizontally)
                 {
-                    Box(modifier = Modifier) {
-                        AsyncImage(
-                            model = photo.sourceURL,
-                            contentDescription = null,
-                            contentScale = ContentScale.FillWidth,
-                            modifier = Modifier
-                                .fillMaxSize(1f)
-                                .padding(16.dp)
-                        )
+                    Box(
+                        modifier = Modifier
+                    ) {
+                        Column {
+                            AsyncImage(
+                                model = photo.sourceURL,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier
+                                    .fillMaxSize(1f)
+                                    .padding(bottom = 20.dp, top = 10.dp)
+                            )
+                            Row {
+                                Icon(
+                                    Icons.Filled.Favorite,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .padding(start = 10.dp)
+                                )
+                                Icon(
+                                    Icons.Filled.Person,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .padding(start = 10.dp)
+                                )
+                                Icon(
+                                    Icons.Filled.Place,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .padding(start = 10.dp)
+                                )
+                                Icon(
+                                    Icons.Filled.Share,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .padding(start = 10.dp)
+                                )
+                            }
+                        }
                     }
-                    Spacer(modifier = Modifier.height(16.dp))
                 }
             },
             confirmButton = {
@@ -156,16 +205,6 @@ fun PhotoCard(photo: Photo, modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun ImageInformation(photo: Photo) {
-    Text(
-        modifier = Modifier
-            .padding(top = 4.dp, bottom = 8.dp)
-            .fillMaxWidth(0.5f),
-        textAlign = TextAlign.Center,
-        text = "Photograph: ${photo.photographer}"
-    )
-}
 @Composable
 fun Photos(searchText: String, results: List<Photo>, updateResults: (String) -> Unit) {
 
@@ -180,7 +219,7 @@ fun Photos(searchText: String, results: List<Photo>, updateResults: (String) -> 
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ){
             items(results) { item ->
-                PhotoCard(item)
+                PhotoCard(item, item.height, item.width)
             }
         }
 }
